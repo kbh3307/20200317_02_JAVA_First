387838dfd25e59f84ade6b497a707cce48e84c21
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-17 14:55:56 +0900

    dataBinding 생성

--- app/src/main/java/kr/co/tjoeun/a20200317_01_login/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_01_login/MainActivity.java
@@ -3,12 +3,32 @@ package kr.co.tjoeun.a20200317_01_login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText inputEdt = null;
+    Button okBtn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        EditText에 타이핑 => 입력버튼 => 텍스트뷰에 출력
+//        + 입력버튼 누르면 기존 입력내용삭제""으로 설정
+        inputEdt = findViewById(R.id.inputEdt);
+        okBtn = findViewById(R.id.okBtn)
+
+        inputEdt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
